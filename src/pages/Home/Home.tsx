c8536bddef065fe644c3715db49e62ede09f4e17
shalexandeer <shalexander09@gmail.com>
2024-04-21 11:56:59 +0700

    Initial Commit

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface HomeProps {
+  props?: any
+}
+
+const Home = ({props}:HomeProps) => {
+  return (
+    <>
+      <p>Home</p>
+    </>
+  )
+}
+
+export default Home
---
+++ src/pages/Result/Result.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface ResultProps {
+  props?: any
+}
+
+const Result = ({props}:ResultProps) => {
+  return (
+    <>
+      <p>Result</p>
+    </>
+  )
+}
+
+export default Result
